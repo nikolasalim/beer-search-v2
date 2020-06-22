5bff425642742f3c9321aaac857806cc26898513
nikolasalim <nikolasalim@gmail.com>
2020-06-22 11:21:27 +0200

    add basic component structure

---
+++ src/app/components/beers-list/beers-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beers-list',
+  templateUrl: './beers-list.component.html',
+  styleUrls: ['./beers-list.component.scss']
+})
+export class BeersListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
